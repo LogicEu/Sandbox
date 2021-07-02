08ef839d1f2ff5c22f5b8bb6173b6ec99ba54b0e
LogicEu <eugenioarteagaa@gmail.com>
2021-07-02 18:33:59 -0400

    Added Aural to compiled libs

--- projects/examples/Aural/example.c
+++ projects/examples/Aural/example.c
@@ -6,7 +6,7 @@ int main(int argc, char** argv)
     int sample_rate = 44100;
     int bit_depth = 16;
 
-    sine_t sine = sine_create(440.0f, 0.5f, sample_rate);
-    wav_file_write_sine("test.wav", sine, 2, sample_rate, bit_depth);
+    sine_t sine = sine_create(523.25f, 0.5f, sample_rate);
+    wav_file_write_sine("test2.wav", sine, 2, sample_rate, bit_depth);
     return 0;
 }
--- src/Aural/src/sine.c
+++ src/Aural/src/sine.c
@@ -2,6 +2,8 @@
 #include <math.h>
 #define TWO_PI 6.28318530718
 
+//https://katyscode.wordpress.com/2013/02/28/cutting-your-teeth-on-fmod-part-5-real-time-streaming-of-programmatically-generated-audio/
+
 sine_t sine_create(float freq, float amp, int sample_rate)
 {
     sine_t sine;
